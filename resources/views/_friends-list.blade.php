43fd3bd33011140b871c76cfb8ffdc0c3aaf04e4
Lea Dalibard <leadalibard@gmail.com>
2021-02-09 11:04:54 +0100

    Add a table Following_user with foreing key and belongsToMany + add methods follows and follow

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,11 +1,11 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 
 <ul>
-    @foreach(range(1, 8) as $index)
+    @foreach(auth()->user()->follows as $user)
     <li>
         <div class="flex items-center text-sm mb-1">
-            <img src="https://i.pravatar.cc/40" alt="avatar" class="rounded-full mr-2">
-            Patti Smith
+            <img src="{{$user->avatar}}">
+           {{$user->name}}
         </div>
     </li>
     @endforeach
