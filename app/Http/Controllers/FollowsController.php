72843e1583f1105c6c97c3c3e45bf23b3862dcfe
Lea Dalibard <leadalibard@gmail.com>
2021-02-10 11:17:28 +0100

    Add working follows button

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class FollowsController extends Controller
 {
-    //
+    public function store(User $user)
+    {
+        //have the auth'd user follow the given user
+        auth()
+            ->user()
+            ->follow($user);
+
+        return back();
+    }
 }
