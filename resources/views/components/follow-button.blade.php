1488b54be8922119a594966aa55a07de094f7cbd
Lea Dalibard <leadalibard@gmail.com>
2021-02-10 13:53:34 +0100

    Add an unless condition so that button follow me not display for own profile'

--- resources/views/components/follow-button.blade.php
+++ resources/views/components/follow-button.blade.php
@@ -1,10 +1,12 @@
-<form method="POST" action="/profiles/{{$user->name}}/follow">
-    @csrf
+@unless (auth()->user()->is($user))
+    <form method="POST" action="/profiles/{{$user->name}}/follow">
+        @csrf
 
-    <button
-        type ="submit"
-        class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs"
-    >
-        {{auth()->user()->following($user)? 'Unfollow Me' : 'Follow-me'}}
-    </button>
-</form>
+        <button
+            type="submit"
+            class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs"
+        >
+            {{auth()->user()->following($user)? 'Unfollow Me' : 'Follow-me'}}
+        </button>
+    </form>
+@endunless
