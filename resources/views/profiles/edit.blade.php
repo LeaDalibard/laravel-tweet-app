cafef189959d1873e23185f9e80ea0077dc035b8
Lea Dalibard <leadalibard@gmail.com>
2021-02-11 10:27:57 +0100

    Displaying Avatar photo in the form

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -41,19 +41,29 @@
             @enderror
 
         </div>
+
         <div class="mb-6">
+
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
                    for="avatar"
             >
-              Avatar
+                Avatar
             </label>
-            <input
-                type="file"
-                class="border border-gray-400 p-2 w-full"
-                name="avatar"
-                id="avatar"
-                required
-            >
+
+            <div class="flex">
+                <input
+                    type="file"
+                    class="border border-gray-400 p-2 w-full"
+                    name="avatar"
+                    id="avatar"
+                    required
+                >
+                <img src="{{$user->avatar}}"
+                     alt="your avatar"
+                     width="40px"
+                >
+            </div>
+
             @error('avatar')
             <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
             @enderror
