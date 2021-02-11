591c421f7745e51ab1f1952a3d26daa2a972134c
Lea Dalibard <leadalibard@gmail.com>
2021-02-11 14:36:49 +0100

    Improving CSS of Remember me pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,79 @@
 <x-master>
 
-    <div class="container mx-auto px-6 py-4 bg-gray-400">
-        <div class="row justify-content-center">
+    <div class="container mx-auto flex justify-center ">
+        <div class="px-6 py-4 bg-gray-300 rounded-3xl">
             <div class="col-md-8">
-
                 <div class="text-md-center font-weight-bold text-lg mb-4">{{ __('Login') }}</div>
+
 
                 <div class="mt-2">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email"
-                                   class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="mb-6">
+                            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                                   for="email"
+                            >
+                                Email
+                            </label>
+                            <input
+                                type="text"
+                                class="border border-gray-400 p-2 w-full"
+                                name="email"
+                                id="email"
+                                autocomplete="email"
+                                value="{{old('email')}}"
+                                required
+                            >
+                            @error('email')
+                            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                            @enderror
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                       name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password"
-                                   class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="mb-6">
+                            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                                   for="password"
+                            >
+                                Password
+                            </label>
+                            <input
+                                type="text"
+                                class="border border-gray-400 p-2 w-full"
+                                name="password"
+                                id="password"
+                                autocomplete="current-password"
+                                value="{{old('password')}}"
+                                required
+                            >
+                            @error('password')
+                            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                            @enderror
 
-                            <div class="col-md-6">
-                                <input id="password" type="password"
-                                       class="form-control @error('password') is-invalid @enderror" name="password"
-                                       required autocomplete="current-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
                         </div>
 
-                        <div class="form-group row mb-6">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember"
-                                           id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <div class="mb-6">
+                         <div>
+                             <input
+                                 type="checkbox"
+                                 class="mr-1"
+                                 name="remember"
+                                 id="remember"
+                                 {{old('remember')?'checked' :''}}
+                             >
+                             <label class="uppercase font-bold text-xs text-gray-700"
+                                    for="remember"
+                             >
+                                 Remember me
+                             </label>
+                         </div>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
+                            @error('remember')
+                            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                            @enderror
+
                         </div>
+
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
